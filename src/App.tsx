e9fd56158fccccb28b92a29d3418b70547e8cadb
Uzaki Toba <yukiuzu0912@gmail.com>
2022-10-20 00:46:35 +0900

    ci: first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+const title = import.meta.env.VITE_APP_TITLE;
+
+export const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
